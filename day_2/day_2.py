ea2fca74a0a5fef21800bacea5882cc3851a8d61
Tolq1n <tolq1n127608@gmail.com>
2024-01-10 23:22:44 +0500

    day_2

---
+++ day_2/day_2.py
@@ -0,0 +1,14 @@
+# 171. Excel Sheet Column Number
+
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        i = len(columnTitle) - 1
+        s = 0
+        for letter in columnTitle:
+            s += (ord(letter)-64)*26**i
+            i -= 1
+        return s
