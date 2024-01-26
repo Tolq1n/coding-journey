aa7fc01dc7a28aaa7e6ae342e42264d963cd59e7
Tolq1n <tolq1n127608@gmail.com>
2024-01-26 21:53:30 +0500

    #263.Ugly Number

---
+++ day_18/day_18.py
@@ -0,0 +1,18 @@
+#263.Ugly Number
+def isUgly(n):
+    if n<=0:return False
+
+    while n != 1:
+        if n%2 == 0:
+            n //= 2
+            continue
+        if n%3 == 0:
+            n //= 3
+            continue
+        if n%5 == 0:
+            n //= 5
+            continue
+        return False
+    return True
+
+print(isUgly(0))
