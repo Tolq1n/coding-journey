66d92975c99e303e146d01606fff24a9273078c5
Tolq1n <tolq1n127608@gmail.com>
2024-02-11 21:43:05 +0500

    #349. Intersection of Two Arrays

---
+++ day_33/day_33.py
@@ -0,0 +1,10 @@
+#349. Intersection of Two Arrays
+def intersection(nums1, nums2):
+    ans = []
+    for num in nums1:
+        if num in nums2 and num not in ans:
+            ans.append(num)
+
+    return ans
+
+print(intersection([1,2,2,1],[2,2]))
