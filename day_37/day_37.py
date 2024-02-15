aa179454bcf7c536e2b3f5204c6a44e60854e7e2
Tolq1n <tolq1n127608@gmail.com>
2024-02-15 23:25:32 +0500

    #2427. Number of Common Factors

---
+++ day_37/day_37.py
@@ -0,0 +1,16 @@
+#2427. Number of Common Factors
+def commonFactors(a, b):
+    s=0
+    if a > b:
+        for i in range(1, a+1):
+            if a%i == 0 and b%i == 0:
+                print('a>b', i)
+                s+=1
+    else:
+        for i in range(1, b+1):
+            if a%i == 0 and b%i == 0:
+                print('b>a', i)
+                s+=1
+    return s
+
+print(commonFactors(25, 30))
