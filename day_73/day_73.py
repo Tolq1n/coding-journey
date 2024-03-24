909e2d49639253d7ecd5a1df0636398e89fcedde
Tolq1n <tolq1n127608@gmail.com>
2024-03-24 21:46:20 +0500

    #645. Set Mismatch

---
+++ day_73/day_73.py
@@ -0,0 +1,8 @@
+#645. Set Mismatch
+def findErrorNums(nums):
+    duplicate_num = sum(nums) - sum(set(nums))
+    expected_sum = sum(range(1, len(nums) + 1))
+    actual_sum = sum(set(nums))
+    missing_num = expected_sum - actual_sum
+
+    return [duplicate_num, missing_num]
